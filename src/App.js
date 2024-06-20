a85f0fcc11fadfe2e4f60fc14e99ae6c0c23e411
akashpandit55900 <akashpandit55900@gmail.com>
2024-06-20 10:01:10 +0530

    Editing HTML, Variables

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+let name = "Akash";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+
+      <div className="container">
+        {/* using variables */}
+        <h1>Hello {name}</h1> 
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut labore assumenda est quaerat, natus eaque aliquid animi maxime omnis non nihil repellat. Magnam, tempora?</p>
+      </div>
+    </>
   );
 }
 
