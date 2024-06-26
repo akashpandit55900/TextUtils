04d091b9b45f0916637fa5a95856c78d00d4b607
akashpandit55900 <akashpandit55900@gmail.com>
2024-06-26 17:43:27 +0530

    Remove Routers - To deploy on github pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React, { useState } from 'react'
 
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
-import About from "./components/About";
+// import About from "./components/About";
 import Alert from './components/Alert';
 
 //React router dom
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 
@@ -47,20 +47,20 @@ function App() {
   }
   return (
     <>
-      <Router>
+      {/* <Router> */}
         <Navbar title="TextUtils" about="About" mode={Mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
             <div className="container my-3">
-                <Routes>
-                    <Route exact path='/' element={
+                {/* <Routes> */}
+                    {/* <Route exact path='/' element={ */}
                         <Textform showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces" mode={Mode} />
-                     }/> 
-                     <Route exact path='/aboutUs' element={ 
-                         <About mode={Mode} /> 
-                     }/> 
-                 </Routes> 
+                     {/* }/>  */}
+                     {/* <Route exact path='/aboutUs' element={  */}
+                         {/* <About mode={Mode} />  */}
+                     {/* }/>  */}
+                 {/* </Routes>  */}
             </div>
-        </Router>
+        {/* </Router> */}
     </>
 );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,18 +8,21 @@ export default function Navbar(props) {
         <>
             <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
                 <div className="container-fluid">
-                    <NavLink  className="navbar-brand fst-italic fw-medium" to="/">{props.title}</NavLink>
+                    {/* <NavLink  className="navbar-brand fst-italic fw-medium" to="/">{props.title}</NavLink> */}
+                    <a  className="navbar-brand fst-italic fw-medium" href="#">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink  exact className="nav-link fw-medium" aria-current="page" to="/">Home</NavLink>
+                                {/* <NavLink  exact className="nav-link fw-medium" aria-current="page" to="/">Home</NavLink> */}
+                                <a  exact className="nav-link fw-medium" aria-current="page" href="#">Home</a>
                             </li>
-                            <li className="nav-item">
-                                <NavLink  exact className="nav-link fw-medium" to="/aboutUs">{props.about}</NavLink>
-                            </li>
+                            {/* <li className="nav-item"> */}
+                                {/* <NavLink  exact className="nav-link fw-medium" to="/aboutUs">{props.about}</NavLink> */}
+                                {/* <a  exact className="nav-link fw-medium" href="/aboutUs">{props.about}</a> */}
+                            {/* </li> */}
                         </ul>
 
 
